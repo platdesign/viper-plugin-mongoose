2ff3ede2c3646a47edacf4b263f71c7c7e5ab7a1
Christian Blaschke <mail@platdesign.de>
2015-02-18 21:29:07 +0100

    refactored

--- index.js
+++ index.js
@@ -1,91 +1,72 @@
 'use strict';
+
 require('mongoose');
 var mongoose 	= require('mongoose-promised');
-var fs 			= require('fs');
-var path 		= require('path');
-var extend		= require('extend');
-var Q 			= require('q');
-
 var schemaExtender = require('./lib/schemaExtender.js');
 
-var defaults = {
-	configId: 'mongoose'
-};
-
-
-var defaultConfig = {
-	modelsPath: './models',
-	options: {}
-};
-
-
 module.exports = function() {
-	var that = this;
 
-	if( this._config[defaults.configId] ) {
+	this.config(function($configProvider, $dbProvider, $q, $is, $path, $cwd, $fs, $log) {
 
-		var config = this._config[defaults.configId]
+		$configProvider.each('mongoose', function(key, item) {
 
-		// Walk config and create connection-provider for each item
-		Object.keys(config).forEach(function(serviceName) {
+			if( $is.string( item.uri ) ) {
 
-			// config args
-			var args = extend(true, {}, defaultConfig, config[serviceName]);
+				$dbProvider.connect(key, function() {
 
-			// Create provider for connection
-			that.provider(serviceName, function() {
+					return $q.promise(function(resolve, reject) {
 
-				var d = Q.defer();
-
-				// create connection
-				var con = mongoose.connect(args.uri, args.options, function(err) {
-					if(err) {
-						// Log failure
-						console.log(('Connection ('+serviceName+') failed. '+err.message).red);
-
-						// reject promise
-						d.reject(err);
-
-						return;
-					}
-
-					// Log success! =)
-					console.log(('Connection ('+serviceName+') has been established successfully.').cyan);
-
-					// Make schemas extendable by default
-					schemaExtender(con);
-
-
-					// Load models from modelsPath
-					var modelsPath = path.resolve( that.cwd(), args.modelsPath );
-					if( fs.existsSync(modelsPath) ) {
-						fs.readdirSync( modelsPath ).forEach(function(item) {
-							var itemPath = path.join( modelsPath, item );
-							var stat = fs.statSync(itemPath);
-							if(stat.isFile()) {
-								var modelFile = require(itemPath);
-								modelFile(con, mongoose.Schema, mongoose.Schema.Types);
+						// create connection
+						var con = mongoose.connect(item.uri, item.options || {}, function(err) {
+							if(err) {
+								// reject promise
+								reject(err);
+								return;
 							}
-						});
-					}
 
-					// resolve with connection
-					d.resolve(con);
+							// Make schemas extendable by default
+							schemaExtender(con);
+
+							// Load models from modelsPath
+							/*
+
+
+
+							*/
+
+							// resolve with connection
+							resolve(con);
+						});
+
+					});
 				});
 
+				if(item.modelsPath) {
+					$dbProvider.onConnected(key, function(con) {
+
+						var modelsPath = $path.resolve( $cwd, item.modelsPath );
+
+						if( $fs.existsSync( modelsPath ) ) {
+							$fs.readdirSync( modelsPath ).forEach(function(item) {
+								var itemPath = $path.join( modelsPath, item );
+								var stat = $fs.statSync(itemPath);
+								if(stat.isFile()) {
+									var modelFile = require(itemPath);
+									modelFile(con, mongoose.Schema, mongoose.Schema.Types);
+									$log.verbose('Mongoose: Loaded model file: '+item.green);
+								}
+							});
+						}
+					});
+				}
 
 
-
-
-				// return service function which returns connection-object
-				return function() {
-					return d.promise;
-				};
-			});
-
+			} else {
+				throw new Error('Missing parameters for database connection: '+key);
+			}
 
 		});
 
-	}
+	});
 
-};
+}
